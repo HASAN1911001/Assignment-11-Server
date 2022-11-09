41c4f027a5411bc943d5ca2a1770a7d41c2620dd
Mr. Zi <md.ziaulhasan00@gmail.com>
2022-11-09 17:35:44 +0600

    service api added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 app.use(cors());
@@ -15,6 +15,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const serviceCollection = client.db('cooked').collection('services');
+        const reviewCollection = client.db('cooked').collection('reviews');
         
         app.get('/services', async(req, res)=>{
             const query = {}
@@ -22,6 +23,33 @@ async function run(){
             const services = await cursor.toArray();
             res.send(services)
         })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        // app.post('/reviews', async(req, res) =>{
+        //     const review = req.body;
+        //     const result = await reviewCollection.insertOne(review);
+        //     res.send(result);
+        // })
+
+        //reviews api
+        // app.get('/reviews', async(req, res) => {
+        //     let query = {};
+        //     if (req.query.email) {
+        //         query = {
+        //             email: req.query.email
+        //         }
+        //     }
+
+        //     const cursor = reviewCollection.find(query);
+        //     const reviews = await cursor.toArray();
+        //     res.send(reviews)
+        // })
     }
     finally{
         
